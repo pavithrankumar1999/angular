773192e581f002839555596d030dfe3761e4d18f
pavithran <pavithrankumar1999@gmail.com>
2021-07-22 12:31:41 +0530

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -14,14 +12,14 @@ import { LogoutComponent } from './logout/logout.component';
 import { AngularMaterialModule } from './angular-material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BasicAuthHtppInterceptorService } from './service/basic-auth-interceptor.service';
+import { ProcesspensionComponent } from './processpension/processpension.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent,
-    AddEmployeeComponent,
     HeaderComponent,
     FooterComponent,
+    ProcesspensionComponent,
     LoginComponent,
     LogoutComponent
   ],
--- src/app/processpension/processpension.component.spec.ts
+++ src/app/processpension/processpension.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddEmployeeComponent } from './add-employee.component';
+import { ProcesspensionComponent } from './processpension.component';
 
-describe('AddEmployeeComponent', () => {
-  let component: AddEmployeeComponent;
-  let fixture: ComponentFixture<AddEmployeeComponent>;
+describe('ProcesspensionComponent', () => {
+  let component: ProcesspensionComponent;
+  let fixture: ComponentFixture<ProcesspensionComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddEmployeeComponent ]
+      declarations: [ ProcesspensionComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AddEmployeeComponent);
+    fixture = TestBed.createComponent(ProcesspensionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
